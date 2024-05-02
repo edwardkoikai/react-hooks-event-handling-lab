500bffaded5f679bdf29a0e5210eca7b6e7fb9b6
edwardkoikai <edwardkoikai@gmail.com>
2024-05-02 13:58:12 +0300

    Add event listener

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function handleChange(event){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" name="password" id="password" onChange={handleChange}/>
+        </div>
     )
 }
 
